8fb319196adce3031bd4649644895762c118c490
rye gaspar <gaspar.ryan@outlook.com>
2019-09-29 19:31:40 -0500

    03 - http requests

---
+++ pages/posts/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <p>{{ posts }}</p>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+    	data() {
+    		return {
+    			posts: []
+            }
+        },
+
+        async asyncData() {
+    		let response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+
+            return {
+    			posts: response.data
+            }
+        }
+
+        // asyncData() {
+    	// 	return axios.get('https://jsonplaceholder.typicode.com/posts').then((response) => {
+    	// 		return {
+    	// 			posts: response.data
+        //         }
+        //     });
+        // }
+
+
+    }
+</script>
